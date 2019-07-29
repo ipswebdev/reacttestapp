34ef7bfa9853d76b20ebaf90a9990d3e29d111ff
ipswebdev <sawant.pranay.15ee1102@gmail.com>
2019-07-29 13:16:01 +0530

    Updates

---
+++ precache-manifest.28faf0c6a968616950bd1ae10ab95826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce8bd8b945700776c144def2753f560",
+    "url": "/reacttestapp/index.html"
+  },
+  {
+    "revision": "745492d4fa6235dc6db7",
+    "url": "/reacttestapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reacttestapp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "745492d4fa6235dc6db7",
+    "url": "/reacttestapp/static/js/main.eaa946e9.chunk.js"
+  },
+  {
+    "revision": "e584225cfc12ff12f02f",
+    "url": "/reacttestapp/static/js/runtime~main.5f7c1427.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttestapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
